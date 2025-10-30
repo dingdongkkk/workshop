a5ad184218dfe7d769c3df742e7388b482c3fe68
onlyanubhav <myacc5010@gmail.com>
2025-10-30 15:33:01 +0530

    added my name

---
+++ done.py
@@ -0,0 +1,2 @@
+name = "anubhav"
+print(name[0:3])
---
+++ newapp.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cin >> "i will be number 1";
+    cout << i will be number 1;
+    return 0;
+
+}
